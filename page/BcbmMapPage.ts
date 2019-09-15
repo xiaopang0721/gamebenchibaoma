7449c02ef90da3bbdf997f7e1bfcf07b5a2952bf
happy <happy>
2019-09-15 21:32:45 +0800

    client批量提交

--- page/BcbmMapPage.ts
+++ page/BcbmMapPage.ts
@@ -206,6 +206,7 @@ module gamebenchibaoma.page {
             this._viewUI.mouseThrough = true;
             this._game.playMusic(Path_game_benchibaoma.music_benchibaoma + "BGM_1.mp3");
             this._viewUI.btn_repeat.disabled = true;
+            this._viewUI.btn_spread.left = this._game.isFullScreen ? 25 : 10;
         }
 
         // 页面打开时执行函数
